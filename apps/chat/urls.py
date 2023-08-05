b68685069fa7c53404a1439e23f51a629298a45b
Romedikc <73655002+luianqi@users.noreply.github.com>
2023-08-05 22:43:04 +0600

    add basename

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ChatBotViewSet
 
 router = DefaultRouter()
-router.register(r'chatbot', ChatBotViewSet)
+router.register(r'chatbot', ChatBotViewSet, basename='ChatMessage')
 
 urlpatterns = [
     path('', include(router.urls)),
