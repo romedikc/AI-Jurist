202c84c0fab4f2f5c2d1bfb0fd9a97399e1098f6
Romedikc <73655002+luianqi@users.noreply.github.com>
2023-08-05 22:43:42 +0600

    rewrite to modelviewsets

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -6,19 +6,13 @@ from apps.chat.serializers import ChatMessageSerializer
 from apps.chat.services import index
 
 
-class ChatBotViewSet(viewsets.ViewSet):
-    def create(self, request):
-        # Get the user's query from the request data
+class ChatBotViewSet(viewsets.ModelViewSet):
+    queryset = ChatMessage.objects.all()
+    serializer_class = ChatMessageSerializer
+
+    def create(self, request, *args, **kwargs):
         user_input = request.data.get('user_input')
-
-        # Call the VectorstoreIndexCreator's query method with the user's query
         bot_response = index.query(user_input)
-
-        # Save the chat history to the database
         chat_message = ChatMessage.objects.create(user_input=user_input, bot_response=bot_response)
-
-        # Serialize the chat message for the response
         serializer = ChatMessageSerializer(chat_message)
-
-        # Return the response as a JSON object
         return Response(serializer.data)
