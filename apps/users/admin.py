6734679337517c9f2ece6f46ac30eed926979cd1
Medet <medik121@gmail.com>
2023-08-06 09:36:42 +0600

    add user and chat models to the admin panel

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.users.models import User
+from apps.chat.models import ChatMessage
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", "username", "phone", "email",
+        "user_type", "is_staff", "is_superuser",
+        "is_active"
+    )
+
+
+@admin.register(ChatMessage)
+class ChatMessage(admin.ModelAdmin):
+    list_display = (
+        'id', 'user_input',
+        'bot_response',
+        'timestamp'
+    )
